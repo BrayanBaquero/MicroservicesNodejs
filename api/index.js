3f496c2ba6ea45101a2c0536fb7eb04c699360cc
Brayan Baquero <brayan.baquero.cz@gmail.com>
2022-04-29 19:03:03 -0500

    Gestion Avanzada de errores:  Throw

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ const express=require('express');
 
 const config=require('../config.js');
 const user=require('./components/user/network');
-const auth=require('./components/auth/network')
+const auth=require('./components/auth/network');
+const errors=require('../network/errors');
 
 const app=express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 
 app.use('/api/user',user);
 app.use('/api/auth',auth);
+
+app.use(errors)
 
 app.listen(config.api.port,()=>{
     console.log('Api escuchando en el puerto ', config.api.port);
--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,7 @@
 const jwt=require('jsonwebtoken');
 const config=require('../config');
+const error=require('../utils/error');
+
 const secret=config.jwt.secret;
 
 function sign(data) {
@@ -14,7 +16,7 @@ const check={
         const decoded=decodeHeader(req);
         console.log(decoded);
         if(decoded.id!==owner){
-            throw new Error('No puedes hacer esto');
+            throw error('No puedes hacer esto',401);
         }
     },
 }
